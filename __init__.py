0e9740c4efbad8af2576f8c0c9e678bafff7d1a6
Claudio Vinicius <claudiovinicius.o@hotmail.com>
2022-07-22 10:06:46 -0300

    Deploy

--- __init__.py
+++ __init__.py
@@ -7,11 +7,17 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 def tick():
     print('Tick! The time is: %s' % datetime.now())
 
+def tick_2():
+    print('Tick! The time is: %s' % datetime.now())
+
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
     scheduler.add_job(tick, 'interval', seconds=3)
     #print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    scheduler.add_job(tick_2, 'interval', seconds=10)
+
 
     try:
         scheduler.start()
--- application.py
+++ application.py
@@ -106,4 +106,31 @@ def main():
     return time.sleep(default_timesleep)
 
 
+
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+def tick():
+    print('Tick! The time is: %s' % datetime.now())
+
+def tick_2():
+    print('Tick! The time is: %s' % datetime.now())
+
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(tick, 'interval', seconds=3)
+    #print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    scheduler.add_job(tick_2, 'interval', seconds=10)
+
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+
+
 main()
